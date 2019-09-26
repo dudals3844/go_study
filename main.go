42cac9f94ab03697215bd24e0ece72ede5be556c
Young Min <dudals3844@gmail.com>
2019-09-26 14:39:44 +0000

    learn for-each infinite loop and break continue

--- main.go
+++ main.go
@@ -7,4 +7,33 @@ func main() {
 	}
 
 	println(sum)
+
+	//infinite loop
+
+	// 	for {
+	// 		println("Infinite loop")
+	// 	}
+
+	//for-each syntax
+	names := []string{"young", "min", "choi"}
+
+	for index, name := range names {
+		println(index, name)
+	}
+
+	////break, continue
+
+	var a = 1
+	for a < 15 {
+		if a == 5 {
+			a += a
+			continue //밑에 코드를 뛰어넘고 다시 27행으로 간다.
+		}
+		a++
+		if a > 10 {
+			break
+		}
+	}
+	print(a)
+
 }
