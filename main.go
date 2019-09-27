bb19fdbc6d951cbf5fb3dc3c211f232cef74070f
Young Min <dudals3844@gmail.com>
2019-09-27 09:47:47 +0000

    learn defince function prototypes used type

--- main.go
+++ main.go
@@ -1,14 +1,20 @@
 package main
 
+type calculator func(int, int) int
+
+func calc(f calculator, a int, b int) int {
+	result := f(a, b)
+	return result
+}
+
 func main() {
-	sum := func(n ...int) int { //익명함수 정의
-		s := 0
-		for _, i := range n {
-			s += i
-		}
-		return s
+	add := func(i int, j int) int {
+		return i + j
 	}
 
-	result := sum(1, 2, 3, 4, 5) //익명함수 호출
-	println(result)
+	r1 := calc(add, 10, 20) //f calculator = add에 있는 익명함수러 정의된다.
+	println(r1)
+
+	r2 := calc(func(x int, y int) int { return x - y }, 10, 20)
+	println(r2)
 }
