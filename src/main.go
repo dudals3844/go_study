15c9b23dd19b24610d66c163d31b945ac3d4a5e4
Young Min <dudals3844@gmail.com>
2019-09-27 23:49:34 +0000

    learn panic function

--- src/main.go
+++ src/main.go
@@ -9,17 +9,18 @@ import (
 
 
 func main() {
-    f , err := os.Open("1.txt")
+    openFile("invalid.txt")
+    println("Done")
+    
+}
+
+func openFile(fn string){
+    f, err := os.Open(fn)
     if err != nil {
-        panic(err)
+        panic(err)//panic함수는 현재함수를 즉시 멈추고 현재함수의 defer함수들을 모두 실행한뒤 즉시 리턴한다.
     }
     
-    //main 마지막에 파일 close
     defer f.Close()
-    bytes := make([]byte, 1024)
-    f.Read(bytes)
-    println(len(bytes))
-    
 }
 
 
