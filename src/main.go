2e2db6d360b2137c4eb50eacd147515348095e83
Young Min <dudals3844@gmail.com>
2019-09-27 14:02:41 +0000

    learn error handling

--- src/main.go
+++ src/main.go
@@ -3,43 +3,30 @@ package main
 import (
     //"fmt"
     //"math"
+    "log"
+    "os"
 )
-//define Rect
-type Rect struct {
-    width, height float64//return type이랑 맞춰줘야 된다.
+type error interface {
+    Error() string
 }
-
-type Circle struct {
-    radius float64
-}
-//Rect 타입에 대한 Shape 인터페이스 구현
-func (r Rect) area2() float64 {
-    
-    return r.width * r.height
-}
-
-// func (r Rect) perimeter() float64{
-//     return 2 * (r.width+r.height)
-// }
-
-// //Circle 타입에 대한 Shape 인터페이스 구현
-// func (c Circle) area() float64{
-//     return math.Pi * c.radius * c.radius
-// }
-// func (c Circle)perimeter()float64{
-//     return 2*math.Pi*c.radius
-// }
 
 func main() {
-    r := Rect{10.,20.}
-    //c := Circle{10.}
-    println(r.area2())
-    //println(c.perimeter())
+    f, err := os.Open("./")
+    if(err != nil){
+       log.Fatal(err.Error())
+    }
+    println(f.Name())
+    
+    _,err := otherFunc()
+    switch err.(type){
+    default: //no error
+        println("ok")
+    case Myerror:
+        log.Print("Log my error")
+    case error:
+        log.Fatal(err.Error())
+            
+    }
 }
 
-// func showArea(shapes ...Shape){
-//     for _,s := range shapes{
-//         a := s.area()//인터페이스 메서드 호출
-//         println(a)
-//     }
-// }
+
