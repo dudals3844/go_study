65e0fd3d74f5113cf0c3180e50d38b44972b10c5
Young Min <dudals3844@gmail.com>
2019-09-27 09:10:30 +0000

    learn function

--- main.go
+++ main.go
@@ -1,39 +1,11 @@
 package main
 
 func main() {
-	sum := 0
-	for i := 0; i <= 100; i++ {
-		sum += i
-	}
+	msg := "Hello"
+	say(msg)
 
-	println(sum)
+}
 
-	//infinite loop
-
-	// 	for {
-	// 		println("Infinite loop")
-	// 	}
-
-	//for-each syntax
-	names := []string{"young", "min", "choi"}
-
-	for index, name := range names {
-		println(index, name)
-	}
-
-	////break, continue
-
-	var a = 1
-	for a < 15 {
-		if a == 5 {
-			a += a
-			continue //밑에 코드를 뛰어넘고 다시 27행으로 간다.
-		}
-		a++
-		if a > 10 {
-			break
-		}
-	}
-	print(a)
-
+func say(msg string) {
+	println(msg)
 }
