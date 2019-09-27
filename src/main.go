3bb281db4beb9edc238bfd2bce9d0b8d5c7d8510
Young Min <dudals3844@gmail.com>
2019-09-27 13:13:20 +0000

    learn declare Struct and create Struct object

--- src/main.go
+++ src/main.go
@@ -2,15 +2,24 @@ package main
 
 import "fmt"
 
+//define struct
+type person struct {
+    name string
+    age int
+}
+
+
 func main() {
-    myMap := map[string]string{
-        "A": "Apple",
-        "B": "Banana",
-        "C": "Charlie",
-    }
+    p := person{}//객체 생성
+    d := new(person)
+    //필드값 설정
+    p.name = "Lee"
+    p.age = 20
     
-    //use for - each
-    for key, val := range myMap {
-        fmt.Println(key,val)
-    }
+    d.name = "Choi"
+    d.age = 21
+    
+    fmt.Println(d)
+    
+    fmt.Println(p)
 }
