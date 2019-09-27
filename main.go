1ef426f696691f4a9b5779fc5dbdcd0e9e62ab5d
Young Min <dudals3844@gmail.com>
2019-09-27 09:50:33 +0000

    learn array

--- main.go
+++ main.go
@@ -1,20 +1,9 @@
 package main
 
-type calculator func(int, int) int
-
-func calc(f calculator, a int, b int) int {
-	result := f(a, b)
-	return result
-}
-
 func main() {
-	add := func(i int, j int) int {
-		return i + j
-	}
+	var a [3]int
+	a[0] = 1
+	a[1] = 2
 
-	r1 := calc(add, 10, 20) //f calculator = add에 있는 익명함수러 정의된다.
-	println(r1)
-
-	r2 := calc(func(x int, y int) int { return x - y }, 10, 20)
-	println(r2)
+	println(a[1])
 }
