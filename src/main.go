f1f435029aa9725c66bb69a0457ffecb023a1c09
Young Min <dudals3844@gmail.com>
2019-09-28 06:56:11 +0000

    learn channel range syntax

--- src/main.go
+++ src/main.go
@@ -1,26 +1,37 @@
 package main
 
 import (
-//"fmt"
-//"math"
-//"log"
-//"os"
-//"time"
-//"sync"
+	//"fmt"
+	//"math"
+	//"log"
+	//"os"
+	//"time"
+	//"sync"
 )
 
 func main() {
 	ch := make(chan int, 2)
-
+	
 	ch <- 1
 	ch <- 2
-
-	close(ch)
-
-	println(<-ch)
-	println(<-ch)
-
-	if _, success := <-ch; !success { //_,success := <-ch은 값을 대입받는다는 의미이다.
-		println("no more data")
+	
+	// 채널이 닫힌 것을 감지할 때까지 계속 수신
+    /*
+    for {
+        if i, success := <-ch; success {
+            println(i)
+        } else {
+            break
+        }
+    }
+    */
+ 
+    // 방법2
+    // 위 표현과 동일한 채널 range 문
+	
+	for i := range ch {
+		println(i)
+	
 	}
+	
 }
