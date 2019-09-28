a2bc91bbf6d264ee0d990a2e79021e8657131686
Young Min <dudals3844@gmail.com>
2019-09-28 03:58:15 +0000

    learn close channel

--- src/main.go
+++ src/main.go
@@ -1,32 +1,26 @@
 package main
 
 import (
-	"fmt"
-	//"math"
-	//"log"
-	//"os"
-	//"time"
-	//"sync"
+//"fmt"
+//"math"
+//"log"
+//"os"
+//"time"
+//"sync"
 )
 
 func main() {
-	ch := make(chan string, 1)
-	sendChan(ch)
-	receiveChan(ch)
+	ch := make(chan int, 2)
+
+	ch <- 1
+	ch <- 2
+
+	close(ch)
+
+	println(<-ch)
+	println(<-ch)
+
+	if _, success := <-ch; !success { //_,success := <-ch은 값을 대입받는다는 의미이다.
+		println("no more data")
+	}
 }
-
-func sendChan(ch chan<- string) {
-	ch <- "Data"
-	//x := <- ch
-}
-
-func receiveChan(ch <-chan string) {
-	data := <-ch
-	fmt.Println(data)
-}
-
-// 해당 채널로 송신만 할 것인지 혹은 수신만할 것인지를 지정할 수도 있다. 송신 파라미터는 (p chan<- int)와 같이 chan<- 을 사용하고,
-// 수신 파라미터는 (p <-chan int)와 같이 <-chan 을 사용한다. 만약 송신 채널 파라미터에서 수신을 한다거나,
-// 수신 채널에 송신을 하게되면, 에러가 발생한다.
-
-// 아래 예제에서 만약 sendChan() 함수 안에서 x := <- ch 를 실행하면 송신전용 채널에 수신을 시도하므로 에러가 발생한다.
