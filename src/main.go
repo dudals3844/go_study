27f165927b446381c7a48c875da5cb1af3bed562
Young Min <dudals3844@gmail.com>
2019-09-28 03:53:38 +0000

    learn go channel parameter

--- src/main.go
+++ src/main.go
@@ -10,13 +10,23 @@ import (
 )
 
 func main() {
-	// c := make(chan int)
-	// c <- 1           //수신루틴이 없어 데드락
-	// fmt.Println(<-c) //별도의 고루틴이 없어서 데드락에 걸린다
-
-	//buffer channel
-	ch := make(chan int, 1) //chan int, 1에서 1이 버퍼의 개수이다.
-	//수신자가 없어도 보낼 수 있다
-	ch <- 101
-	fmt.Println(<-ch)
+	ch := make(chan string, 1)
+	sendChan(ch)
+	receiveChan(ch)
 }
+
+func sendChan(ch chan<- string) {
+	ch <- "Data"
+	//x := <- ch
+}
+
+func receiveChan(ch <-chan string) {
+	data := <-ch
+	fmt.Println(data)
+}
+
+// 해당 채널로 송신만 할 것인지 혹은 수신만할 것인지를 지정할 수도 있다. 송신 파라미터는 (p chan<- int)와 같이 chan<- 을 사용하고,
+// 수신 파라미터는 (p <-chan int)와 같이 <-chan 을 사용한다. 만약 송신 채널 파라미터에서 수신을 한다거나,
+// 수신 채널에 송신을 하게되면, 에러가 발생한다.
+
+// 아래 예제에서 만약 sendChan() 함수 안에서 x := <- ch 를 실행하면 송신전용 채널에 수신을 시도하므로 에러가 발생한다.
